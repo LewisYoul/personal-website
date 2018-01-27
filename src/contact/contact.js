0eb3c8d8ceb0f8857ef8d0add40359eed749766e
Lewis <lewisyoul@gmail.com>
2018-01-27 19:29:47 +0000

    Import Blog post titles from medium rss feed (parse as JSON)

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react'
+import Feed from 'rss-to-json'
 
 class Contact extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentWillMount(){
+    let self = this
+    Feed.load('https://cors-anywhere.herokuapp.com/https://medium.com/feed/@lewisyoul', function(err, rss) {
+      console.log(rss.items)
+      self.setState({
+        posts: rss.items
+      });
+      console.log(self.state)
+    });
+  }
+
 
   render(){
+    const allPosts = this.state.posts.map((post, index) => {
+      return(
+        <h1>{post.title}</h1>
+      )
+    });
+
     return(
       <div className="component text-center">
         <h1><strong><span>[</span> Contact <span>]</span></strong></h1>
         <hr></hr>
         <div className="row">
           <div className="col-md-10 col-sm-offset-1 text-left">
-            <h3><strong>Who am I?</strong></h3>
+            {allPosts}
             <hr></hr>
-            <p>Before embarking on a career in web development I worked as a scientist studying the microbiota of animals. Using the analysis program R prompted me to delve farther into the coding depths. I have since spent some time honing my skills in development, pecifically through learning Ruby in preparation for Makers Academy which I will begin in October.</p>
+            <p></p>
           </div>
         </div>
       </div>
